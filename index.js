19180f33affaf6876d5d64ce0e9c1433b620d77f
Manish <manishjbangera@gmail.com>
2021-03-15 04:55:52 +0530

    bug fix:removed resutls object

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let list = document.getElementById("taravelInfo").children;
 const form = document.getElementById("taravelInfo");
 let ids = {};
-let results = {};
 const invalid = {
     "name":"start with alphabets,Enter your valid name.",
     "email":"invalid email ",
     "number":"invalid mobile number ",
     "address":"please fill the valid address"
 }
-
 
 Array.from(list).forEach(element => {
     element.addEventListener("change",validate);
@@ -21,22 +19,19 @@ function validate(event){
    if(name === "name"){
        let regex = /^[a-z]{1,2}\w+(\s+?\w+){0,10}/i;
        if(regex.test(element.value)){
-            isValid =true;
-            
+            isValid =true;    
        }
    }
    else if(name === "email"){
         let regex = /(\w+)[@]([a-z]+)[.]([a-z]+)/i;
-        if(regex.test(element.value)){
-          
+        if(regex.test(element.value)){ 
             isValid =true;
         }
    }
 
    else if(name === "number"){
        let regex = /\b\d{10}\b/;
-       if(regex.test(event.target.value)){
-            
+       if(regex.test(event.target.value)){  
             isValid =true;
         }
    }
@@ -59,8 +54,8 @@ function validate(event){
             ...ids,
             [name]:name
         }
-         element.classList.add("is-valid");
-         return;
+        element.classList.add("is-valid");
+        return;
        }
        else{
         element.classList.remove("is-valid");
@@ -73,20 +68,15 @@ function validate(event){
             [name]:name
         }
 
-        results = {
-            ...results,
-            [name]:true
-        }
         element.classList.remove("is-invalid");
         element.classList.add("is-valid");
         let feedback = document.getElementById("f"+name);
         feedback.classList.replace("invalid-feedback","valid-feedback");
         feedback.innerText = "looks good..";
    }else{
-        results = {
-            ...results,
-            [name]:false
-        }
+
+        delete ids[name];
+        console.log({ids});
         element.classList.add("is-invalid");
         let feedback = document.getElementById("f"+name);
         feedback.classList.replace("valid-feedback","invalid-feedback");
@@ -110,9 +100,7 @@ function result(value,message){
             })
         },0)
         form.reset();
-        ids = {};
-        results = {};
-        
+        ids = {};    
     }else{
         document.querySelector(".container").insertAdjacentHTML("beforebegin",html);
     }
@@ -127,7 +115,7 @@ function result(value,message){
 form.addEventListener("submit",(event)=>{
     
     event.preventDefault();
-    let arr = Object.values(results);
+    let arr = Object.values(ids);
     if(check(arr)){
         result("success","Your travel request has been successfully submited");
     }else{
@@ -137,14 +125,10 @@ form.addEventListener("submit",(event)=>{
 });
 
 function check(arr){
-
-    if(arr.length < 5 ){
+    
+    console.log(arr);
+    if(arr.length < 6){
         return false;
-    }
-    for(value in arr){
-        if(value===false){
-            return false;
-        }
     }
     return true;
 }
